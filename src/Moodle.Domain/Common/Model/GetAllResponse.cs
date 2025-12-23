420e9fa9f5638ef7100ecd7486cd5c3b7470adb7
nduje <dujerancic@gmail.com>
2025-12-23 16:41:21 +0100

    Add common response and result models

---
+++ src/Moodle.Domain/Common/Model/GetAllResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Moodle.Domain.Common.Model
+{
+    public class GetAllResponse<TValue>
+    {
+        public IEnumerable<TValue> Values { get; init; }
+
+        public GetAllResponse(IEnumerable<TValue> values)
+        {
+            Values = values;
+        }
+
+        public GetAllResponse()
+        {
+            Values = Enumerable.Empty<TValue>();
+        }
+    }
+}
---
+++ src/Moodle.Domain/Common/Model/Result.cs
@@ -0,0 +1,16 @@
+﻿using Moodle.Domain.Common.Validation;
+
+namespace Moodle.Domain.Common.Model
+{
+    public class Result<TValue>
+    {
+        public TValue Value { get; private set; }
+        public ValidationResult ValidationResult { get; private set; }
+
+        public Result(TValue value, ValidationResult validationResult)
+        {
+            Value = value;
+            ValidationResult = validationResult;
+        }
+    }
+}
